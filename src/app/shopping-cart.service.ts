d3c6bdbe9bc5f3c21f763c4afb2019c65ab5bf6a
Nguyen Huu Luan <luan.nguyen@swissitbridge.local>
2017-11-29 19:03:40 +0700

    implement get quantity

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -14,7 +14,8 @@ export class ShoppingCartService {
     });
   }
 
-  private getCart(cartId: string) { 
+  async getCart() { 
+    let cartId = await this.getOrCreateCartId();
     return this.db.object('/shopping-carts/' + cartId);
   }
 
